086e0a09de07acca794e07d498480ea0eb08930e
ctmackay <charles.mackay@coram.ai>
2024-04-09 13:24:37 -0700

    make it more realistic

---
+++ src/template.py
@@ -0,0 +1,8 @@
+from data_structs import Settings
+
+PROD_EARLY_ADOPTER_SETTINGS = Settings(
+    gittag="REPLACE_ME_GIT_TAG",
+    gitsha="REPLACE_ME_GIT_SHA",
+    config_folder="REPLACE_ME_CONFIG_FOLDER",
+    sls_filename="REPLACE_ME_SLS_FILENAME",
+)
