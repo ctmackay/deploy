9e6044449afecd60e1d159a1efb074ce0e6b85b5
ctmackay <ctmackay@users.noreply.github.com>
2024-04-17 00:52:18 +0000

    release update

--- src/settings_release.py
+++ src/settings_release.py
@@ -1,8 +1,8 @@
 from data_structs import Settings
 
-PROD_EARLY_ADOPTER_SETTINGS = Settings(
-    gittag="v16.03.01",
-    gitsha="427844187c76f41ccaaa90b99d80f1455e638228",
+RELEASE_ADOPTER_SETTINGS = Settings(
+    gittag="zaa9",
+    gitsha="20efadafade1ca3bc373eed25eba9e539f366c9f",
     config_folder="logistics-core-early",
     sls_filename="prod_early_adopter.sls",
 )
