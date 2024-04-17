65f875c90d1efa8b2d46f449cfa9e77811929df0
ctmackay <ctmackay@users.noreply.github.com>
2024-04-17 00:48:27 +0000

    Prerelease update

--- src/settings_early.py
+++ src/settings_early.py
@@ -1,8 +1,8 @@
 from data_structs import Settings
 
 PROD_EARLY_ADOPTER_SETTINGS = Settings(
-    gittag="zaa1",
-    gitsha="zaashag",
+    gittag="zaa8",
+    gitsha="088231dfb3f1215417df0a479327afa208ff1362",
     config_folder="logistics-core-early",
     sls_filename="prod_early_adopter.sls",
 )
